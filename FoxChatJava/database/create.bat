cf15b7c8ab920e2cf765885b14b642bb4f2f31aa
Kolton Nay <koltonnay@outlook.com>
2020-10-01 15:56:28 -0400

    Removed the java server to another repo for deployment.

--- FoxChatJava/database/create.bat
+++
@@ -1,6 +0,0 @@
-ECHO OFF
-psql -U postgres -f "dropdb.sql"
-createdb -U postgres final_capstone
-psql -U postgres -d final_capstone -f "schema.sql"
-psql -U postgres -d final_capstone -f "user.sql"
-PAUSE
